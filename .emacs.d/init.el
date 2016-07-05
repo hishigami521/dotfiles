4537d29ca6eb5e0fc961fc07f5264a1796f94662
Hiroyuki ISHIGAMI <hishigam@yahoo-corp.jp>
2016-07-05 20:34:58 +0900

    update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -213,12 +213,11 @@
 (require 'auto-complete)
 (require 'auto-complete-config)
 (global-auto-complete-mode t)
-(add-to-list 'ac-modes 'text-mode)
 (add-to-list 'ac-modes 'fundamental-mode)
 (add-to-list 'ac-modes 'org-mode)
 (add-to-list 'ac-modes 'yatex-mode)
 (add-to-list 'ac-modes 'nxml-mode)
-(ac-set-trigger-key "TAB")
+`(ac-set-trigger-key "TAB")
 (setq ac-delay 0.1)
 (setq ac-use-menu-map t)
 (define-key ac-menu-map "\C-n" 'ac-next)
@@ -261,24 +260,43 @@
 
 ;; ------------------------------------------------------------------------
 ;; @ spell check
-;; (setq-default ispell-program-name "aspell")
-;; (eval-after-load "ispell"
-;;   '(add-to-list 'ispell-skip-region-alist '("[^\000-\377]+")))
-;; ;; comment ここに書いたモードではコメント領域のところだけ
-;; (mapc ;; only for comments
-;;  (lambda (hook)
-;;    (add-hook hook 'flyspell-prog-mode))
-;;  '(
-;;    c-mode-common-hook
-;;    emacs-lisp-mode-hook
-;;    ))
-;; (mapc ;; always 
-;;  (lambda (hook)
-;;    (add-hook hook
-;;              '(lambda () (flyspell-mode 1))))
-;;  '(
-;;    yatex-mode-hook
-;;         ))
+(setq-default ispell-program-name "aspell")
+(eval-after-load "ispell"
+  '(add-to-list 'ispell-skip-region-alist '("[^\000-\377]+")))
+;; comment ここに書いたモードではコメント領域のところだけ
+(mapc ;; only for comments
+ (lambda (hook)
+   (add-hook hook 'flyspell-prog-mode))
+ '(
+   c-mode-common-hook
+   emacs-lisp-mode-hook
+   sh-mode-hook
+   ))
+(mapc ;; always 
+ (lambda (hook)
+   (add-hook hook
+             '(lambda () (flyspell-mode 1))))
+ '(
+   yatex-mode-hook
+        ))
+
+;; ------------------------------------------------------------------------
+;; @ makefile-mode
+(autoload 'makefile-mode "makefile-bsdmake-mode"
+  "Major mode for editing Makefile files" t)
+(add-to-list 'auto-mode-alist '("Makefile" . makefile-bsdmake-mode))
+(add-to-list 'auto-mode-alist '("Makefile\\..*$" . makefile-bsdmake-mode))
+(add-to-list 'auto-mode-alist '("Makefile_\\..*$" . makefile-bsdmake-mode))
+(add-to-list 'auto-mode-alist '("\\.mk\\'" . makefile-bsdmake-mode))
+(add-to-list 'ac-modes 'makefile-bsdmake-mode)
+
+;; ------------------------------------------------------------------------
+;; @ shell-script-mode
+(autoload 'shell-script-mode "sh-mode"
+  "Major mode for editing Shell-Script files" t)
+(add-to-list 'auto-mode-alist '("[.]zsh.*" . sh-mode))
+;; (add-to-list 'auto-mode-alist '("\\.zshrc[.|_].*" . sh-mode))
+(add-to-list 'ac-modes 'sh-mode)
 
 ;; ------------------------------------------------------------------------
 ;; @ markdown-mode
@@ -289,6 +307,7 @@
 (add-to-list 'auto-mode-alist '("\\.txt\\'" . markdown-mode))
 ;; (setq auto-mode-alist
 ;;          (cons '("\\.text" . markdown-mode) auto-mode-alist))
+(add-to-list 'ac-modes 'markdown-mode)
 
 ;; ------------------------------------------------------------------------
 ;; @ C-mode
--- Makefile
+++ Makefile
@@ -1,10 +1,65 @@
-all: emacs
+DOT=~/dotfiles
 
-emacs_link:
-	ln -sf ~/dotfiles/.emacs.d/init.el ~/.emacs.d/init.el;
-	ln -sf ~/dotfiles/.emacs.d/Cask ~/.emacs.d/Cask;
+all: 
+setting:
+	inst_brew
+	inst_from_brew
+	set_zsh
+	set_aspell
+	set_git
+	set_vim
+	set_emacs
 
-link:
-	ln -sf ~/dotfiles/.emacs.d ~/.emacs.d;
-	ln -sf ~/dotfiles/.zshrc ~/.zshrc;
-	ln -sf ~/dotfiles/.vimrc ~/.vimrc;
+inst_software: inst_brew inst_from_brew inst_emacs
+
+inst_cask:
+	bash Install/install_cask.sh;
+	echo 'export PATH="$HOME/.cask/bin:$PATH"' >> .zshrc.local;
+	cask upgrade
+
+inst_brew:
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
+
+inst_from_brew:
+	brew install zsh zsh-completions;
+	brew install gcc make ncurses-devel; # for emacs 
+	brew install wget automake autoconf
+	brew install markdown;
+	brew install coreutils;
+
+inst_emacs:
+	cd /usr/local; 
+	wget http://ftp.jaist.ac.jp/pub/GNU/emacs/emacs-24.5.tar.gz;
+	tar xvf emacs-24.5.tar.gz;
+	cd emacs-24.5;
+	./configure;
+	sudo make;
+	sudo make install;
+	cd ${DOT};
+
+set_emacs:
+	# bash Install/set_emacs.sh;
+	# setting .emacs.d
+	dir=~/.emacs.d; [ ! -e $dir ] && mkdir $dir;
+	histdir=$dir/cache/savehist; [ ! -e $histdir ] && mkdir -p $histdir;
+	ln -sf ~/dotfiles/.emacs.d/init.el $dir/init.el;
+	ln -sf ~/dotfiles/.emacs.d/Cask $dir/Cask;
+
+	# package install
+	cd $dir;
+	cask install;
+	cd -;
+set_vim:
+	ln -sf ${DOT}/.vimrc ${HOME}/;
+
+set_zsh:
+	ln -sf ${DOT}/.zshrc ${HOME}/;
+	[ -f .zshrc_`uname` ] && cp .zshrc_`uname` ${HOME}/.zshrc.local;
+
+set_git:
+	brew install git;
+	echo 'export PATH="/usr/local/Cellar/git/2.9.0/bin:$PATH"' >> .zshrc.local;
+
+set_aspell:
+	brew install aspell;
+	echo "lang en_US" > ~/.aspell.conf;
